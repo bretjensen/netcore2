e4a53b6028f458f2e401f67e105f366cc9674e1c
Brett Jensen <brett@adduco.cdigital>
2018-02-27 18:53:31 +0000

    @initial@

---
+++ auth/Services/IuserService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AspNetCore.Auth.Web.Services
+{
+  public interface IUserService
+  {
+    Task<bool>  ValidateCredentials(string username, string password, out User user);
+  }
+
+  public class User 
+  {
+    public User(string username)
+    {
+        
+    }
+
+    public string Username { get; set; }
+  }
+}
